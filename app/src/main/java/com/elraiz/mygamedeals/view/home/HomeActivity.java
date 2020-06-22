e98fe50c752c5be3c0c52fc649fa702ac6ec5f8d
elraiz <raizrock161@gmail.com>
2020-06-22 07:56:54 +0700

    add home screen

---
+++ app/src/main/java/com/elraiz/mygamedeals/view/home/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.elraiz.mygamedeals.view.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.elraiz.mygamedeals.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
